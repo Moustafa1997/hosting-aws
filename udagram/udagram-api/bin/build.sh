3fa07b45842b2c5f044935acbe2d63ae69a76c37
moustafa1997 <moustafa.aboulazm1997@gmail.com>
2022-10-19 19:30:26 +0200

    host

---
+++ udagram/udagram-api/bin/build.sh
@@ -0,0 +1,1 @@
+npm run clean && tsc && cp -rf src/config www/config && cp -R .elasticbeanstalk www/.elasticbeanstalk && cp .npmrc www/.npmrc && cp package.json www/package.json && cd www && zip a  -tzip  Archive.zip . && cd ..
