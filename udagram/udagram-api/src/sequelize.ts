e6082fa1d85ceec2c5c33b244a01f29284a1de79
moustafa1997 <moustafa.aboulazm1997@gmail.com>
2022-10-21 15:25:25 +0200

    host

--- udagram/udagram-api/src/sequelize.ts
+++ udagram/udagram-api/src/sequelize.ts
@@ -6,7 +6,7 @@ export const sequelize = new Sequelize({
   password: config.password,
   database: config.database,
   host: config.host,
-  port: config.dbport,//for server
+  port: config.port,//for server
 
   dialect: "postgres",
   storage: ":memory:",
