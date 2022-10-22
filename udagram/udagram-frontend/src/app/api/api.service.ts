319d47d24cc34c52be9446164fc4572badcccf0a
moustafa1997 <moustafa.aboulazm1997@gmail.com>
2022-10-22 03:20:38 +0200

    host

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -2,9 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpRequest, HttpEvent } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { map } from 'rxjs/operators';
+var cors = require('cors')
+
+var app :any = express();
+
 
 const API_HOST = environment.apiHost;
+app.use(function (req, res, next) {
 
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use(cors());
 @Injectable({
   providedIn: 'root'
 })
@@ -73,3 +95,7 @@ export class ApiService {
     });
   }
 }
+function express() {
+  throw new Error('Function not implemented.');
+}
+
