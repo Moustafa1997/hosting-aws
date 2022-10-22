df6b50598cbc951bb04ec6f9c4afc06b7994abc8
moustafa1997 <moustafa.aboulazm1997@gmail.com>
2022-10-22 03:44:59 +0200

    host

--- udagram/udagram-frontend/src/app/api/api.service.ts
+++ udagram/udagram-frontend/src/app/api/api.service.ts
@@ -10,18 +10,13 @@ var app :any = express();
 
 
 const API_HOST = environment.apiHost;
-app.use(function (req, res, next) {
+const x=app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
   res.setHeader('Access-Control-Allow-Credentials', true);
 
@@ -34,7 +29,7 @@ app.use(function (req, res, next) {
 })
 export class ApiService {
   httpOptions = {
-    headers: new HttpHeaders({'Content-Type': 'application/json','Access-Control-Allow-Origin': 'PUT'})
+    headers: new HttpHeaders({'Content-Type': 'application/json',x})
   };
 
   token: string;
